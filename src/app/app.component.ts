21341623a6245fae8fd536d8cb24f92a3a5464dc
Joao Victor Borges <borges.jvdo@gmail.com>
2025-01-23 17:19:11 -0300

    feat: add initial page links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'links.io';
+
+  groups = [
+    {
+      description: 'Links',
+      links: [
+        {
+          label: 'Github',
+          url: 'https://github.com/victorborges97'
+        },
+        {
+          label: 'Linkedin',
+          url: 'https://www.linkedin.com/in/victorborges97/'
+        }
+      ]
+    }
+  ]
 }
